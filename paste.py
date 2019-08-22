7a668f97d8d4f12a15b6b2e7133e4414a4757b3d
nvelleman <nvelleman@outbrain.com>
2019-08-23 00:08:40 +0300

    comments

--- paste.py
+++ paste.py
@@ -6,6 +6,7 @@ class Paste:
         self.content = content
         self.date = date
         # self.date = time.format('YYYY-MM-DD_HH:mm')  should convert to UTC
+        # defaults values - take care
 
     def __repr__(self):
         return "Title: {}\nAuthor: {}\nDate: {}\nContent: {}\n\n".format(self.title, self.author, self.date, self.content)
