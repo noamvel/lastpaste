d7e53c6d02f4a3ed08509a088f083af1321978c3
nvelleman <nvelleman@outbrain.com>
2019-08-22 23:09:55 +0300

    default None

--- lastpaste.py
+++ lastpaste.py
@@ -20,25 +20,41 @@ def parse_pastes_hrefs(archive_document):
     # filtering pastes older than 2 minutes
     # filtering 'syntax' hrefs
     post_time_filter = "[tr/td/text() = '1 min ago' or tr/td/text() = '2 min ago' or contains(tr/td/text(), 'sec ago')]"
-    pastes_table = archive_document.xpath("//table[@class='maintable']" + post_time_filter)[0]
+    pastes_table_list = archive_document.xpath("//table[@class='maintable']" + post_time_filter)
+    if pastes_table_list:
+        pastes_table = pastes_table_list[0]
+    else:
+        return [];
     pastes = pastes_table.xpath("tr/td/a[not(contains(@href, 'archive'))]/@href")
-    print(len(pastes))
+    print('{} new pastes'.format(len(pastes)))
     return pastes
 
 
 def parse_paste(paste_document):
     # parse paste meta_data & content
-    title = paste_document.xpath("//div[@class='paste_box_line1']/@title")[0].strip()
-    print(title)
-    author = paste_document.xpath("//div[@class='paste_box_line2']/img")[0].tail.strip()
-    print(author)
-    date = paste_document.xpath("//span/@title")[0]
-    print(date)
-
-
-'''
-
-'''
+    title = None
+    author = None
+    date = None
+    content = None
+    title_list = paste_document.xpath("//div[@class='paste_box_line1']/@title")
+    if title_list:
+        title = title_list[0].strip()
+    box_line2_element_list = paste_document.xpath("//div[@class='paste_box_line2']")
+    if box_line2_element_list:
+        box_line2_element = box_line2_element_list[0]
+        author_list = box_line2_element.xpath("img")
+        date_list = box_line2_element.xpath("span/@title")
+        if author_list:
+            author = author_list[0].tail.strip()
+        if date_list:
+            date = date_list[0]
+    paste = {
+        'title': title,
+        'author': author,
+        'date': date,
+        'content': content
+    }
+    return paste
 
 
 def write_to_file(pastes, time):
@@ -58,7 +74,9 @@ for href in pastes_hrefs:
     print(paste_url)
     paste_doc = get_document(paste_url)
     # async?
-    parse_paste(paste_doc)
+    paste_model = parse_paste(paste_doc)
+    print(paste_model)
+    #write_to_file(paste_model, time)
 
 
 
