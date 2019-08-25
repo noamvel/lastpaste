839e65b0e7f5ed1bcf0796eafd1ae018e15c6289
nvelleman <nvelleman@outbrain.com>
2019-08-25 15:15:26 +0300

    file writing

--- latestpastes.py
+++ latestpastes.py
@@ -6,20 +6,21 @@ import logging
 logging.basicConfig(filename='latestpastes.log', level=logging.INFO)
 time = arrow.utcnow().format('YYYY-MM-DD_HH:mm')
 logging.info('\nJob {} started'.format(time))
-
 filename = 'pbjobs/{}_pastes.txt'.format(time)
-file = open(filename, "a")
 
 archive = PastebinArchiveClient().get()
 hrefs = parse_archive(archive)
-logging.info('Writing to File {}'.format(file.name))
-for href in hrefs:
-    paste_txt = PastebinSinglePasteClient(href).get()
-    paste = parse_paste(paste_txt)
-    print(paste)
-    file.write(paste.__repr__())
-file.close()
-logging.info('Finished\n')
+
+with open(filename, 'w+') as writer:
+    logging.info('Writing to File {}'.format(filename))
+    for href in hrefs:
+        paste_txt = PastebinSinglePasteClient(href).get()
+        paste = parse_paste(paste_txt)
+        print(paste)
+        writer.write(paste.__repr__())
+    writer.truncate()
+
+logging.info('Job Finished Successfully\n')
 
 
 
