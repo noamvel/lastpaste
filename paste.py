4fb1f7824820d3cfee8152baaa2eb0cb9851849e
nvelleman <nvelleman@outbrain.com>
2019-08-25 18:06:28 +0300

    __normalized

--- paste.py
+++ paste.py
@@ -1,12 +1,20 @@
 class Paste:
 
+    __normalized_author = ('a guest', 'Guest', 'Unknown', 'Anonymous', None)
+    __normalized_title = ('Unknown', 'Untitled', None)
+
     def __init__(self, title, author, date, content):
-        self.author = author
-        self.title = title
+        self.author = self.__normalized(author, Paste.__normalized_author);
+        self.title = self.__normalized(title, Paste.__normalized_title);
         self.content = content
         self.date = date
         # self.date = time.format('YYYY-MM-DD_HH:mm')  should convert to UTC
-        # defaults values - take care
+
+    @staticmethod
+    def __normalized(val, normalized_val):
+        if val in normalized_val:
+            return ""
+        return val
 
     def __repr__(self):
         return "Title: {}\nAuthor: {}\nDate: {}\nContent:\n{}\n\n".format(self.title, self.author, self.date, self.content)
--- pbclient/pastebin_client.py
+++ pbclient/pastebin_client.py
@@ -2,34 +2,34 @@ import sys
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 import logging
 
 
 class PastebinClient:
 
-    pb_home_url = 'https://pastebin.com'
-    timeout = 1
-    retries = 3
-    backoff_factor = 0.8
-    status_forcelist = (500, 502, 504)
+    __pb_home_url = 'https://pastebin.com'
+    __timeout = 1
+    __retries = 3
+    __backoff_factor = 0.8
+    __status_forcelist = (500, 502, 504)
     retry_policy = Retry(
-        total=retries,
-        read=retries,
-        connect=retries,
-        backoff_factor=backoff_factor,
-        status_forcelist=status_forcelist,
+        total=__retries,
+        read=__retries,
+        connect=__retries,
+        backoff_factor=__backoff_factor,
+        status_forcelist=__status_forcelist,
     )
 
     def __init__(self, relative_path=None):
-        self.url = PastebinClient.pb_home_url + relative_path
+        self.url = PastebinClient.__pb_home_url + relative_path
         self.adapter = HTTPAdapter(max_retries=PastebinClient.retry_policy)
 
     def get(self):
         session = requests.Session()
         session.mount(self.url, self.adapter)
         try:
-            response = session.get(self.url, timeout=PastebinClient.timeout)
+            response = session.get(self.url, timeout=PastebinClient.__timeout)
             response.raise_for_status()
         except Exception as e:
             self.handle_exception(e)
@@ -41,15 +41,15 @@ class PastebinClient:
 
     @classmethod
     def set_retries(cls, retries):
-        cls.retries = retries
+        cls.__retries = retries
 
     @classmethod
     def set_timeout(cls, timeout):
-        cls.timeout = timeout
+        cls.__timeout = timeout
 
     @classmethod
     def set_backoff_factor(cls, backoff_factor):
-        cls.backoff_factor = backoff_factor
+        cls.__backoff_factor = backoff_factor
 
 
 class PastebinArchiveClient(PastebinClient):
@@ -57,8 +57,8 @@ class PastebinArchiveClient(PastebinClient):
     def __init__(self):
         super().__init__('/archive')
 
-    def handle_exception(self, exception):
-        logging.error("'Archive page currently unavailable.\n {}\nExiting ...'".format(exception))
+    def handle_exception(self, e):
+        logging.error("'Archive page currently unavailable.\n {}\nExiting ...'".format(e))
         sys.exit(1)
 
 
@@ -67,8 +67,8 @@ class PastebinSinglePasteClient(PastebinClient):
     def __init__(self, href):
         super().__init__(href)
 
-    def handle_exception(self, exception):
-        logging.error("'Paste page currently unavailable.\n {}\nSkipping ...'".format(exception))
+    def handle_exception(self, e):
+        logging.error("'Paste page currently unavailable.\n {}\nSkipping ...'".format(e))
         return None
 
 
