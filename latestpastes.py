be99384fd736d500b24b3255d4030a835e67b3b5
nvelleman <nvelleman@outbrain.com>
2019-08-26 13:51:58 +0300

    scheduler

--- latestpastes.py
+++ latestpastes.py
@@ -1,3 +1,5 @@
+import threading
+
 from pbclient.pastebin_client import PastebinArchiveClient, PastebinSinglePasteClient
 from pbparser.pastebin_parser import parse_archive, parse_paste
 import logging
@@ -6,33 +8,39 @@ import time
 import schedule
 
 
+def run_job(job):
+    job_thread = threading.Thread(target=job)
+    job_thread.start()
+
+
+def latest_pastes():
+    time_now = arrow.utcnow()
+    time_sec = time_now.format('YYYY-MM-DD HH:mm:ss')
+    time_min = time_now.format('YYYY-MM-DD_HH:mm')
+    logging.info('\nJob {} started'.format(time_sec))
+    filename = 'pbjobs/pastes_{}.txt'.format(time_min)
+
+    archive = PastebinArchiveClient().get()
+    hrefs = parse_archive(archive)
+    with open(filename, 'w') as writer:
+        logging.info('{} new pastes found'.format(len(hrefs)))
+        logging.info('Writing new pastes to File {}'.format(filename))
+        write_counter = 0
+        for href in hrefs:
+            paste_txt = PastebinSinglePasteClient(href).get()
+            paste = parse_paste(paste_txt)
+            if paste:
+                writer.write(paste.__repr__())
+                write_counter += 1
+                print(paste)
+    logging.info('Job Finished, {} new pastes written.\n'.format(write_counter))
+
+
 logging.basicConfig(filename='latestpastes.log', level=logging.INFO)
-
-
-now = arrow.utcnow().format('YYYY-MM-DD_HH:mm')
-logging.info('\nJob {} started'.format(now))
-filename = 'pbjobs/{}_pastes.txt'.format(now)
-
-archive = PastebinArchiveClient().get()
-hrefs = parse_archive(archive)
-
-with open(filename, 'w') as writer:
-    logging.info('{} new pastes found'.format(len(hrefs)))
-    logging.info('Writing to File {}'.format(filename))
-    write_counter = 0
-    for href in hrefs:
-        paste_txt = PastebinSinglePasteClient(href).get()
-        paste = parse_paste(paste_txt)
-        if paste:
-            writer.write(paste.__repr__())
-            write_counter += 1
-            print(paste)
-logging.info('Job Finished, {} new pastes written.\n'.format(write_counter))
-
-
-
-
-
-
-
-
+schedule.every(2).minutes.at(":00").do(run_job, latest_pastes)
+while True:
+    try:
+        schedule.run_pending()
+        time.sleep(1)
+    except Exception as e:
+        logging.error("Exiting 'latestpastes'.\n{} ".format(e))
