97a9bab6d95448a7962c97013cd20fdda5d71931
nvelleman <nvelleman@outbrain.com>
2019-08-28 05:03:26 +0300

    parser refactor

--- pbparser/pastebin_parser.py
+++ pbparser/pastebin_parser.py
@@ -4,35 +4,35 @@ import logging
 
 
 def parse_archive(archive_text):
-    # parse pastes hrefs from 'Pastes Archive'
-    # filtering pastes older than 2 minutes
-    # filtering 'syntax' hrefs
-    archive_doc = html.document_fromstring(archive_text)
-    post_time_filter = "[(td/text() = '1 min ago' or contains(td/text(), 'sec ago'))]"
-    filtered_tr_list = archive_doc.xpath(".//table[@class='maintable']/tr{}".format(post_time_filter))
+    doc = html.document_fromstring(archive_text)
+    time_filter = "[(td/text() = '1 min ago' or contains(td/text(), 'sec ago'))]"
+    trs = doc.xpath(f".//table[@class='maintable']/tr{time_filter}")
     hrefs = []
-    for e in filtered_tr_list:
-        try:
-            href = e.xpath('td[1]/a/@href')[0]
+    for tr in trs:
+        href = _get_element(tr, 'td[1]/a/@href')
+        if href:
             hrefs.append(href)
-        except Exception as e:
-            logging.error("'Failed to parse 'href'.\n {}\nSkipping ...'".format(e))
     return hrefs
 
 
-def parse_paste(paste_text):
-    # parse paste meta_data & content
-    if not paste_text:
+def _get_element(element, path):
+    if element is None:
         return None
     try:
-        paste_doc = html.document_fromstring(paste_text)
-        title = paste_doc.xpath(".//div[@class='paste_box_line1']/@title")[0]
-        box_line2_element = paste_doc.xpath(".//div[@class='paste_box_line2']")[0]
-        author = box_line2_element.xpath('img')[0].tail
-        date = box_line2_element.xpath('span/@title')[0]
-        content = paste_doc.xpath("//textarea[@id='paste_code']/text()")[0]
+        return element.xpath(path)[0]
     except Exception as e:
-        logging.error("'Failed to parse 'paste'.\n {}\nSkipping ...'".format(e))
+        logging.error(f'Failed to parse {element}:{path}\n{e}\nSkipping')
         return None
+
+
+def parse_paste(paste_text):
+    if not paste_text:
+        return None
+    doc = html.document_fromstring(paste_text)
+    title = _get_element(doc, ".//div[@class='paste_box_line1']/@title")
+    content = _get_element(doc, "//textarea[@id='paste_code']/text()")
+    box_line2 = _get_element(doc, ".//div[@class='paste_box_line2']")
+    author = _get_element(box_line2, 'img').tail
+    date = _get_element(box_line2, 'span/@title')
     paste = Paste(title, author, date, content)
     return paste
